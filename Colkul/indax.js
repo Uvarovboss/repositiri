407df3223481cd1f13418daf032bb94d3c5b0ba4
Uvarovboss <you@example.com>
2018-01-29 13:20:09 +0300

    first_commit

---
+++ Colkul/indax.js
@@ -0,0 +1,17 @@
+var firstNumber;
+var secondNumber;
+
+firstNumber = Number(prompt("input first number"));
+secondNumber = Number(prompt("input second number"));;
+
+var sum = firstNumber + secondNumber;
+var sub = firstNumber - secondNumber;
+var sud = firstNumber * secondNumber;
+var suw = firstNumber / secondNumber;
+
+
+
+document.write( + sum + "<br>");
+document.write( + sub + "<br>");
+document.write( +sud + "<br>");
+document.write( +suw);
